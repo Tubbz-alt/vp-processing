7e2d6ef0dfc0524124a6003982b53c564cfe4794
Peter Desmet <peter.desmet.work@gmail.com>
2017-05-12 00:10:34 +0200

    Create function to get specific data & metadata from vp object as df

---
+++ src/vp_to_df.R
@@ -0,0 +1,57 @@
+#' Get select metadata and data from a vertical bird profile as a data.frame
+#' @description This function creates a data.frame from a vertical bird profile 
+#' (vp). The data.frame contains:
+#' * `vp$attributes$what`: NOD:radar_id, datatime (repeated for every HGHT)
+#' * `vp$data`: HGHT, u, v, dens, sd_vpp
+#' 
+#' For documentation on vp attributes, see: 
+#' https://github.com/adokter/vol2bird/wiki/ODIM-bird-profile-format-specification
+#' 
+#' @param vp bioRad 'vp' object (vertical profile)
+#' @return data.frame with select metadata and data of that vp (records = HGHTs)
+#' @examples
+#' {
+#' vp_to_df(vp)
+#' }
+vp_to_df <- function(vp) {
+  # Select data from the vp$data
+  df <- subset(vp$data, select = 
+    c("HGHT", "u", "v", "dens", "sd_vvp")
+  )
+  
+  # Select metadata from vp$attributes/what/
+  # Get radar_id "seang" from "NOD:seang", which is the first element in 
+  # attributes/what/source
+  source <- unlist(strsplit(vp$attributes$what$source, ","))
+  radar_id <- unlist(strsplit(source[1], ":"))[2]
+  
+  # Get and combine attributes/what/date & attributes/what/time
+  datetime_string <- paste0(vp$attributes$what$date, vp$attributes$what$time)
+  # Express as an ISO datetime
+  datetime <- format(
+    as.POSIXct(datetime_string, format = "%Y%m%d%H%M%S"),
+    "%Y-%m-%dT%H:%M:%SZ"
+  )
+  
+  # Prepend metadata to data.frame
+  df <- cbind(
+    "radar_id" = radar_id,
+    "datetime" = datetime,
+    df
+  )
+  return(df)
+}
+
+get_metadata <- function(vp) {
+  # Get and split attributes/what/source
+  source <- unlist(strsplit(vp$attributes$what$source, ","))
+  # Get radar_id part ("seang") from first source element ("NOD:seang"):
+  radar_id <- unlist(strsplit(source[1], ":"))[2]
+  
+  # Get and combine attributes/what/date & attributes/what/time
+  datetime_string <- paste0(vp$attributes$what$date, vp$attributes$what$time)
+  # Express as an ISO datetime
+  datetime <- format(as.POSIXct(datetime_string, format = "%Y%m%d%H%M%S"), "%Y-%m-%dT%H:%M:%SZ")
+  
+  return(list("radar_id" = radar_id, "datetime" = datetime))
+}
