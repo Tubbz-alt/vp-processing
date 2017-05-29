d0f418d0653222a380ce3f8d32926681a4c8bd72
Peter Desmet <peter.desmet.work@gmail.com>
2017-05-29 16:16:03 +0200

    Select specific heights
    No need to select on general heights anymore

--- src/functions/load_settings.R
+++ src/functions/load_settings.R
@@ -8,10 +8,10 @@ load_settings <- function(settings_file) {
   # settings[["raw_settings"]] <- yaml_settings
 
   # Get general settings
-  general_start_date <- yaml_settings$general$include_dates[1]
-  general_end_date <- yaml_settings$general$include_dates[2]
-  general_min_height <- yaml_settings$general$include_heights[1]
-  general_max_height <- yaml_settings$general$include_heights[2]
+  general_start_date <- yaml_settings$general$include_dates[[1]]
+  general_end_date <- yaml_settings$general$include_dates[[2]]
+  general_min_height <- yaml_settings$general$include_heights[[1]]
+  general_max_height <- yaml_settings$general$include_heights[[2]]
 
   # Save general settings
   settings$general$start_date <- general_start_date
@@ -27,6 +27,19 @@ load_settings <- function(settings_file) {
     # Throw an error if radar ID does not contain 5 characters
     # Expected is e.g "searl" (countrycode + odim code)
     if (nchar(radar_id) != 5) stop(paste0("Radar ID should have 5 characters: ", radar_id))
+
+    # Save radar id
+    settings$radars[[radar_id]]$radar_id <- radar_id
+
+    # Get radar height settings
+    radar_min_height <- yaml_settings$radars[[radar_id]]$include_heights[[1]]
+    radar_max_height <- yaml_settings$radars[[radar_id]]$include_heights[[2]]
+
+    # Save radar height settings
+    # If the value is anything other than an integer (e.g. when no heights were
+    # defined for that radar = NULL), use the general height settings
+    settings$radars[[radar_id]]$min_height <- if (is.integer(radar_min_height)) radar_min_height else general_min_height
+    settings$radars[[radar_id]]$max_height <- if (is.integer(radar_max_height)) radar_max_height else general_max_height
   }
 
   # Save radar ids
