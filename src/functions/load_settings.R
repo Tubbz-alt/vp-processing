5bf4585066353b3143de0dfc5b522f764ff41c09
Peter Desmet <peter.desmet.work@gmail.com>
2017-05-29 14:35:56 +0200

    Group settings

--- src/functions/load_settings.R
+++ src/functions/load_settings.R
@@ -5,25 +5,34 @@ load_settings <- function(settings_file) {
   settings <- list()
 
   # Copy original settings
-  # settings[["yaml_settings"]] <- yaml_settings
+  # settings[["raw_settings"]] <- yaml_settings
 
   # Get general settings
-  settings[["start_date"]] <- yaml_settings$general$include_dates[1]
-  settings[["end_date"]] <- yaml_settings$general$include_dates[2]
-  settings[["min_height"]] <- yaml_settings$general$include_heights[1]
-  settings[["max_height"]] <- yaml_settings$general$include_heights[2]
+  general_start_date <- yaml_settings$general$include_dates[1]
+  general_end_date <- yaml_settings$general$include_dates[2]
+  general_min_height <- yaml_settings$general$include_heights[1]
+  general_max_height <- yaml_settings$general$include_heights[2]
 
-  # Get radar ids
-  radar_ids <- names(yaml_settings$radars)
-  # Throw error if radar id does not contain 5 characters
-  # Expected is e.g "searl" (countrycode + odim code)
+  # Save general settings
+  settings$general$start_date <- general_start_date
+  settings$general$end_date <- general_end_date
+  settings$general$min_height <- general_min_height
+  settings$general$max_height <- general_max_height
+
+  # Get radar settings
+  radars <- yaml_settings$radars
+  radar_ids <- names(radars)
+
   for (radar_id in radar_ids) {
-    if (nchar(radar_id) != 5)
-      stop(paste0("Radar ID should have 5 characters: ", radar_id))
+    # Throw an error if radar ID does not contain 5 characters
+    # Expected is e.g "searl" (countrycode + odim code)
+    if (nchar(radar_id) != 5) stop(paste0("Radar ID should have 5 characters: ", radar_id))
   }
-  settings[["radar_ids_5char"]] <- radar_ids
-  settings[["radar_ids_3char"]] <- substr(radar_ids,3,5) # e.g. "arl" from "searl"
-  settings[["countries"]] <- unique(substr(radar_ids,1,2)) # e.g. "se" from "searl" (unique values only)
+
+  # Save radar ids
+  settings$general$radar_ids_5char <- radar_ids
+  settings$general$radar_ids_3char <- substr(radar_ids,3,5) # e.g. "arl" from "searl"
+  settings$general$countries <- unique(substr(radar_ids,1,2)) # e.g. "se" from "searl" (unique values only)
 
   return(settings)
 }
